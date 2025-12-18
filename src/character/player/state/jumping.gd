df9c01d1b67c7f23c57c27ec25e6f4d3f166348c
Bee Akin <you@example.com>
2025-12-18 11:59:44 -0800

    Update jumping.gd

--- src/character/player/state/jumping.gd
+++ src/character/player/state/jumping.gd
@@ -1,8 +1,14 @@
 class_name PlayerJumpingState extends PlayerState
 
 @onready var falling_state: State = $"../Falling"
+@onready var glomping_state: State = $"../Glomping"
 
 func on_enter(_previous_state: State, data := {}) -> void:
+	# dont let us buffer a jump when glomping something!
+	if check_for_glomping() and not player.glomped_body:
+		goto(glomping_state, data)
+		return
+
 	player.gravity_enabled = true
 	player.move_enabled = true
 
